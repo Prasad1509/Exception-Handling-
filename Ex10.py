3c19e11d9ba749b7f0b044f802ea0147ccc531ec
Prasad Shinde <prasadshinde150904@gmail.com>
2026-05-09 15:59:28 +0530

    Update Ex10.py

--- Ex10.py
+++ Ex10.py
@@ -7,7 +7,7 @@ def check_account(account_number):
     valid_accounts = ["1111", "2222", "3333"]
     if account_number not in valid_accounts:
         raise InvalidAccountNumber("This account number doesn't exist.")
-    else:
+    else: 
         print("Account found. You can proceed.")
 
 
