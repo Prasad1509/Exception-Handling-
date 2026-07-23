38ffd0719376acc34b1769184bfba2a1b4798426
Prasad Shinde <prasadshinde150904@gmail.com>
2026-07-23 09:37:37 +0530

    Update Ex7.py

--- Ex7.py
+++ Ex7.py
@@ -3,7 +3,7 @@ class ResultTooLargeError(Exception):
     pass
 def divide_numbers(a, b):
     try:
-        result = a / b
+        result = a / b 
         if result > 100:
             raise ResultTooLargeError("The result is too large!")
     except ZeroDivisionError:
